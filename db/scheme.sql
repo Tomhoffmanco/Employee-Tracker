cee2bcb4b351c597c7220be70e77e7a2c6750836
Tom hoffman <tomhoffmanco@gmail.com>
2022-06-25 21:24:43 -0400

    create db & tables

--- db/scheme.sql
+++ db/scheme.sql
@@ -1,19 +1,33 @@
-DROP TABLE IF EXISTS
+DROP TABLE IF EXISTS employees
 CREATE DATABASE employees;
 
 use employees;
 
 
-CREATE TABLE departments (
-    id INTEGAR AUTO_INCREMENT PRIMARY KEY, 
-    department_NAME   VARCHAR(30) NOT NULL
-)
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
 
-CREATE TABLE role(
+CREATE TABLE role (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL, 
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_deparment FOREIGN KEY (department_id) REGERENCES department(id) ON DELETE CASCADE
 
 )
 
 CREATE TABLE employees (
-
-)
+        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        role_id INT UNSIGNED NOT NULL,
+        index role_ind (role_id),
+        CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE, 
+        manager_id INT UNSIGNED,
+        INDEX MAN_IND (manager_id),
+        CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL 
+);
 
